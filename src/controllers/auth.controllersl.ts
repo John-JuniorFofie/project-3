202bd1175b5b4f9f9b9d3bf7638084a7c6a0ba18
John-JuniorFofie <johnfofie31@gmail.com>
2025-10-16 18:37:32 +0000

    modified:   src/controllers/auth.controllersl.ts

--- src/controllers/auth.controllersl.ts
+++ src/controllers/auth.controllersl.ts
@@ -1,11 +1,11 @@
 import type {Request, Response} from "express";
 import jwt from 'jsonwebtoken'
 import bcrypt from 'bcrypt';
-import {User} from '../models/user.model.ts'
+import type{User} from '../models/user.model.ts'
 import generateOTP from '../Utils/OTP';
 import OTPverification from '../models/OTPverification..model';
-import {CustomJwtPayload} from '../types/authRequest';
-import {SendEmail} from '../Utils/email.transporter';
+import type{CustomJwtPayload} from '../types/authRequest.ts';
+import type{SendEmail} from '../Utils/email.transporter.ts';
 
 require ('dotenv').config();
 
