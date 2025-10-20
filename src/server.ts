e32368d486471fd1e4636229a9a7ca110261ced4
John-JuniorFofie <johnfofie31@gmail.com>
2025-10-20 18:57:13 +0000

    modified:   src/server.ts

--- src/server.ts
+++ src/server.ts
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 5000;
 
 // CORS configuration
 // app.use(cors(
-//   {
+//   { jj
 //     origin:(_origin, callback)=>callback(null,true),
 //     credentials:true,
 //   }
